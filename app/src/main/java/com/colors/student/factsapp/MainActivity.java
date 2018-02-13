73cb24933886833bfaab69dd89732410c6a263f0
Helena Zvirbule <helenazvirbule@gmail.com>
2018-02-13 10:25:59 +0200

    Upcast to API 24

---
+++ app/src/main/java/com/colors/student/factsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.colors.student.factsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
