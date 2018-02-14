bf3e1d1fbe126c960e692b1dc1915744752eb730
Helena Zvirbule <helenazvirbule@gmail.com>
2018-02-14 16:52:04 +0200

    FactActivity back to menu btn works, improved fact_view

--- app/src/main/java/com/colors/student/factsapp/FactActivity.java
+++ app/src/main/java/com/colors/student/factsapp/FactActivity.java
@@ -16,18 +16,24 @@ import android.widget.TextView;
 public class FactActivity extends AppCompatActivity {
     private ShareActionProvider mShareActionProvider;
 
-
-    TextView category;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.fact_view);
-        category = findViewById(R.id.category);
-
+        Intents intents = new Intents(this);
+        TextView category = findViewById(R.id.category);
         Intent intent = getIntent();
         String message = intent.getExtras().getString("category");
         category.setText(message);
         ImageButton share = findViewById(R.id.shareBtn);
+        ImageButton toMenu = findViewById(R.id.backBtn);
+
+        toMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intents.mainMenu);
+            }
+        });
 
         share.setOnClickListener(new View.OnClickListener() {
             @Override
