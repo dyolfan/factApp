1316ebb85bfba9e07b3da6c9221993d562a23768
MrVitols <martins.vitols89@gmail.com>
2018-02-19 15:48:50 +0200

    completed tests for class Fact

--- app/src/test/java/com/colors/student/factsapp/databases/FactTest.java
+++ app/src/test/java/com/colors/student/factsapp/databases/FactTest.java
@@ -29,6 +29,19 @@ public class FactTest {
 
     @Test
     public void compareTo() throws Exception {
+            int output1 = -1;
+            int output2 = 1;
+            int output3 = 0;
+            Fact fact = new Fact(4,"Wood is si doow wood is si dooW",5);
+            Fact fact1 = new Fact(1,"Bla bla bla bla bla",6);
+            Fact fact2 = new Fact(2,"Bla bla bla bla bla",4);
+            Fact fact3 = new Fact(3,"Bla bla bla bla bla",5);
+            int result1 = fact.compareTo(fact1);
+            int result2 = fact.compareTo(fact2);
+            int result3 = fact.compareTo(fact3);
+            assertEquals(output1,result1);
+            assertEquals(output2,result2);
+            assertEquals(output3,result3);
     }
 
 }
