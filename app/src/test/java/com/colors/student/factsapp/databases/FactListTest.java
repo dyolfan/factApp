24887edd51cb0085e6008e6572d00b53cd2dfb94
MrVitols <martins.vitols89@gmail.com>
2018-02-22 10:35:02 +0200

    Changed factListTest object creation with new parametrs.

--- app/src/test/java/com/colors/student/factsapp/databases/FactListTest.java
+++ app/src/test/java/com/colors/student/factsapp/databases/FactListTest.java
@@ -1,24 +1,29 @@
 package com.colors.student.factsapp.databases;
 
+
 import org.junit.Test;
 import org.junit.Assert;
 import static org.junit.Assert.*;
 
-/**
- * Created by student on 2/19/18.
- */
+
 public class FactListTest {
     @Test
-    public void addFact() throws Exception {
-    Fact fact = new Fact(4, "Wood is si doow, bla bla bla",15);
+    public void userAddFact() throws Exception {
+    Fact fact = new Fact( "Wood is si doow, bla bla bla",15, "ww");
 
     }
-
     @Test
-    public void getFact() throws Exception {
+    public void getFact() throws Exception{
+        FactList factlist = new FactList();
+        Fact fact = new Fact("Ball is round", 7,"tt");
+        Fact fact1 = new Fact("Ball is rounder", 8,"rr");
+        Fact fact2 = new Fact("Ball is rounderer", 9,"ee");
+        factlist.sports.add(fact);
+        factlist.sports.add(fact1);
+        factlist.sports.add(fact2);
+        assertNotNull(factlist.getFact("Sports"));
 
     }
-
     @Test
     public void sortTopFacts() throws Exception {
 
