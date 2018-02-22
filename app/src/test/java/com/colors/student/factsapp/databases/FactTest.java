760afb01d79e1de5b98c98e5c98aabe49a743f95
MrVitols <martins.vitols89@gmail.com>
2018-02-22 14:33:21 +0200

    Created another Fact constructor for testing purposes

--- app/src/test/java/com/colors/student/factsapp/databases/FactTest.java
+++ app/src/test/java/com/colors/student/factsapp/databases/FactTest.java
@@ -7,9 +7,18 @@ import static org.junit.Assert.*;
 
 public class FactTest {
     @Test
+    public void getKey() throws Exception {
+        Fact fact = new Fact("This is fact.","ooo");
+        String output;
+        String expected = "ooo";
+        output = fact.getKey();
+        assertEquals(output,expected);
+    }
+
+    @Test
     public void getText() throws Exception {
         String output;
-        Fact fact = new Fact("Wood is si doow",34,"uu");
+        Fact fact = new Fact("Wood is si doow");
         String expected = "Wood is si doow";
         output = fact.getText();
         assertEquals(output, expected);
@@ -18,7 +27,7 @@ public class FactTest {
     @Test
     public void getShortText() throws Exception {
             String output;
-            Fact fact = new Fact("Wood is si doow Wood is si doow", 34,"jj");
+            Fact fact = new Fact("Wood is si doow Wood is si doow");
             String expected = "Wood is si doo... ";
             output = fact.getShortText();
             assertEquals(output, expected);
@@ -30,10 +39,10 @@ public class FactTest {
             int output1 = -1;
             int output2 = 1;
             int output3 = 0;
-            Fact fact = new Fact("Wood is si doow wood is si dooW",5,"yu");
-            Fact fact1 = new Fact("Bla bla bla bla bla",6,"ty");
-            Fact fact2 = new Fact("Bla bla bla bla bla",4,"io");
-            Fact fact3 = new Fact("Bla bla bla bla bla",5,"oi");
+            Fact fact = new Fact("Wood is si doow wood is si dooW", 5);
+            Fact fact1 = new Fact("Bla bla bla bla bla", 6);
+            Fact fact2 = new Fact("Bla bla bla bla bla", 4);
+            Fact fact3 = new Fact("Bla bla bla bla bla", 5);
             int result1 = fact.compareTo(fact1);
             int result2 = fact.compareTo(fact2);
             int result3 = fact.compareTo(fact3);
