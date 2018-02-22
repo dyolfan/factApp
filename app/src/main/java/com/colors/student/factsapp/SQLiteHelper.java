7328eed0812e1e2803cd1bc409925dc410457e77
Pēteris Bērziņš <peeeca7@gmail.com>
2018-02-22 12:33:21 +0200

    Created tests for Favoutites/SQLite, covered all back-end functions - add/delete/select.

--- app/src/main/java/com/colors/student/factsapp/SQLiteHelper.java
+++ app/src/main/java/com/colors/student/factsapp/SQLiteHelper.java
@@ -22,11 +22,12 @@ public class SQLiteHelper  extends SQLiteOpenHelper {
 
     public SQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+       // database = this.getWritableDatabase();
     }
 
 
     String TABLE_NAME = "Favourites";
-    String COLUMN_AUTO_ID = "favId";
+    String COLUMN_AUTO_ID = "favId";//Auto incremented id, may be useful later
     String COLUMN_FACT = "favFact";
 
     @Override
@@ -57,21 +58,20 @@ public class SQLiteHelper  extends SQLiteOpenHelper {
         database.close();
     }
 
-    //Deletes column from DB where id is equal our given id, for example: id of the fact user just unfavourited
+    //Deletes column from DB where fact text is equal our given fact text(string)
     public void deleteRecord(FavouritesModel contact) {
         database = this.getWritableDatabase();
         database.execSQL("delete from " + TABLE_NAME + " where " + COLUMN_FACT + " = '" + contact.getFact() + "';");
         database.close();
     }
 
-    public void remakeTable(){
+    public void deleteAllRecords(){
         database = this.getWritableDatabase();
-        database.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        //onCreate(database);
+        database.execSQL("delete from "+ TABLE_NAME);
     }
 
     //Selects favourite facts, I have tuned this so it returns all database objects with parameters:
-    // OriginalID, Fact text, Category number
+    // ID, Fact text
     public List<FavouritesModel> getAllRecords() {
         database = this.getWritableDatabase();
         Cursor cursor = database.rawQuery("SELECT * FROM " + TABLE_NAME, null);
@@ -88,7 +88,7 @@ public class SQLiteHelper  extends SQLiteOpenHelper {
             } while (cursor.moveToNext());
         }
         cursor.close();
-        //database.close();
+        database.close();
         return facts;
     }
 
@@ -108,71 +108,9 @@ public class SQLiteHelper  extends SQLiteOpenHelper {
             } while (cursor.moveToNext());
         }
         cursor.close();
-        //database.close();
+        database.close();
         if(facts.isEmpty())return false;
         return true;
-
     }
 
 }
-
-
-/*
-    String TABLE_NAME = "Favourites";
-    String COLUMN_AUTO_ID = "favId";
-    String COLUMN_OG_ID = "id";
-    String COLUMN_FACT = "favFact";
-    String COLUMN_CATEGORY = "category";
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + " ( " + COLUMN_AUTO_ID +
-                " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_OG_ID + " INTEGER, "+ COLUMN_FACT + " VARCHAR, " + COLUMN_CATEGORY + " INTEGER);");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
-    }
-
-
-
-    //Inserts object (the "Fact" we just favourited) info into our database,
-    // contact is the newly created fact aka Class FavouritesModel object
-    public void insertRecord(FavouritesModel contact) {
-        database = this.getReadableDatabase();
-        database.execSQL("INSERT INTO " + TABLE_NAME + "(" + COLUMN_FACT + "," + COLUMN_CATEGORY + "," + COLUMN_OG_ID + ") VALUES('" + contact.getFact() + "','" + contact.getCategory()+ "','" + contact.getID() + "')");
-        database.close();
-    }
-
-    //Deletes column from DB where id is equal our given id, for example: id of the fact user just unfavourited
-    public void deleteRecordAlternate(FavouritesModel contact) {
-        database = this.getReadableDatabase();
-        database.execSQL("delete from " + TABLE_NAME + " where " + COLUMN_OG_ID + " = '" + contact.getID() + "'");
-        database.close();
-    }
-
-    //Selects favourite facts, I have tuned this so it returns all database objects with parameters:
-    // OriginalID, Fact text, Category number
-    public ArrayList<FavouritesModel> getAllRecordsAlternate() {
-        database = this.getReadableDatabase();
-        Cursor cursor = database.rawQuery("SELECT * FROM " + TABLE_NAME, null);
-        ArrayList<FavouritesModel> contacts = new ArrayList<FavouritesModel>();
-        FavouritesModel contactModel;
-        if (cursor.getCount() > 0) {
-            for (int i = 0; i < cursor.getCount(); i++) {
-                cursor.moveToNext();
-                contactModel = new FavouritesModel();
-                contactModel.setID(cursor.getInt(1));
-                contactModel.setFact(cursor.getString(2));
-                contactModel.setCategory(cursor.getInt(3));
-                contacts.add(contactModel);
-            }
-        }
-        cursor.close();
-        return contacts;
-    }
-}
-
-*/
---
+++ app/src/test/java/com/colors/student/factsapp/AddToFavouritesTest.java
@@ -0,0 +1,9 @@
+package com.colors.student.factsapp;
+
+/**
+ * Created by student on 18.22.2.
+ */
+
+public class AddToFavouritesTest {
+
+}
